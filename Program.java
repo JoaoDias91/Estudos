424242787ee150c1baf52ee8a59d6dcdc23ad26c
João Paulo Dias Siqueira <joaodiassiqueira@gmail.com>
2017-05-19 05:08:26 -0300

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,12 @@
+public class Program
+{
+    public static void main(String[] args) {
+        String word1, word2;
+        word1 = "hello";
+        word2 = "world";
+        
+        System.out.println (word1 +" "+ word2);
+        
+        System.out.println ("hello world");
+    }
+}
