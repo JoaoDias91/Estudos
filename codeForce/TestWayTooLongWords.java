07a4c76abf707f9646f098a9001ec7d5547d6711
joao <joaodiassiqueira@gmail.com>
2021-05-11 23:21:07 -0300

    WIP WayTooLongWords

--- codeForce/TestWayTooLongWords.java
+++ codeForce/TestWayTooLongWords.java
@@ -6,6 +6,11 @@ public class TestWayTooLongWords {
         String word = scan.nextLine();
         System.out.println(word);
         scan.close();
-        
+        char[] wordToCharArray =  word.toCharArray();
+        String finalWord = Character.toString(word.charAt(0)) + 
+                       String.valueOf(word.substring(1, wordToCharArray.length -1).length()) + 
+                       Character.toString(word.charAt(word.length() - 1));
+        System.out.println(finalWord);
     }
 }
+
--- codeForce/WayTooLongWords.java
+++ codeForce/WayTooLongWords.java
@@ -1,19 +1,43 @@
 import java.util.Scanner;
 
 public class WayTooLongWords {
-    int quantity;
-    String [] words;
+    /*A quantidade de palavras a serem abreviadas deve ser definida pelo inteiro inserido na primeira linha 
+        ex
+        4
+        word
+        localization
+        internationalization
+        pneumonoultramicroscopicsilicovolcanoconiosis
 
+        saida:
+        
+        word
+        l10n
+        i18n
+        p43s
+
+        */
+    
     public static void main(String[] args) {
+        //t0do Tentar trocar o array de String por um ArrayList
         Scanner scan = new Scanner(System.in);
-        String [] input = new String [5];
+        String [] input = new String [100];
         for(int i = 0; i < 5; i++){
              input[i] = scan.nextLine();
         }
-        int quantityOfWords = Integer.valueOf(input[0]);
-        for(int i = 1; i <= 5; i++){
+        String [] modifiedWords = new String[input.length - 1];
+        for(int i = 1, j = 0; i <= 5; i++, j++){
             if(input[i].length() > 10){
-                
+                String word = input[i];
+                char[] wordToCharArray =  word.toCharArray();
+                String modifiedWord = Character.toString(word.charAt(0)) + 
+                       String.valueOf(word.substring(1, wordToCharArray.length -1).length()) + 
+                       Character.toString(word.charAt(word.length() - 1));
+                modifiedWords[j] = modifiedWord; 
+                //trata com String e add na lista 
+            }else{
+                modifiedWords[j] = input[i];
+                //add na lista somente
             }
        }
         scan.close(); 
